47ffa1707b644e6fd587a0d45d9745ab0953d3ee
James Westby <james@jameswestby.net>
2024-07-15 12:12:30 +0100

    Some more tests for the build command

--- tests/test_build.rs
+++ tests/test_build.rs
@@ -142,12 +142,12 @@ fn test_build_rebuilds_if_file_changes() {
         .modified()
         .unwrap();
 
-    // Tiny sleep to make sure the timestamp changes
-    thread::sleep(Duration::from_nanos(500));
+    cmd.assert().success();
 
     test_context.workdir.child("hello").touch().unwrap();
 
-    cmd.assert().success();
+    // Tiny sleep to make sure the timestamp changes
+    thread::sleep(Duration::from_nanos(500));
 
     eprintln!(
         "{}",
@@ -163,4 +163,68 @@ fn test_build_rebuilds_if_file_changes() {
         .unwrap();
 
     assert!(ending_timestamp > middle_timestamp);
+}
+
+#[test]
+fn test_error_when_is_not_an_arfifact() {
+    let config_src = r#"
+        [command.exec.copy]
+        command = "cp hello world"
+    "#;
+
+    let test_context = common::TestContext::new();
+    test_context.write_config(config_src);
+
+    let mut cmd = test_context.get_command();
+    cmd.arg("build").arg("copy");
+
+    cmd.assert().failure();
+
+    cmd.assert().stderr(predicate::str::contains(
+        "Target <copy> is not buildable, use the run command instead",
+    ));
+}
+
+#[test]
+fn test_error_when_does_not_exist() {
+    let config_src = r#"
+        [command.exec.copy]
+        command = "cp hello world"
+    "#;
+
+    let test_context = common::TestContext::new();
+    test_context.write_config(config_src);
+
+    let mut cmd = test_context.get_command();
+    cmd.arg("build").arg("non_existent");
+
+    cmd.assert().failure();
+
+    cmd.assert().stderr(predicate::str::contains(
+        "Target <non_existent> not found in config file <",
+    ));
+}
+
+#[test]
+fn test_error_when_ambiguous() {
+    let config_src = r#"
+        [artifact.exec.copy]
+        command = "cp hello world"
+
+        [artifact.container_image.copy]
+        context = "."
+        tag = "latest"
+    "#;
+
+    let test_context = common::TestContext::new();
+    test_context.write_config(config_src);
+
+    let mut cmd = test_context.get_command();
+    cmd.arg("build").arg("copy");
+
+    cmd.assert().failure();
+
+    cmd.assert().stderr(predicate::str::contains(
+        "Target <copy> is ambiguous, possible values are <artifact.exec.copy, artifact.container_image.copy>",
+    ));
 }
