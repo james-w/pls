a5b3fa76b1ecf05d206f1627fc06e3ac49598d15
James Westby <james@jameswestby.net>
2025-11-29 12:06:14 +0000

    Add aliases for run and list

--- tests/test_list.rs
+++ tests/test_list.rs
@@ -24,3 +24,25 @@ fn test_list() {
         "artifact.exec.copy - \ncommand.container.hello - Hello world\n",
     ));
 }
+
+#[test]
+fn test_list_alias_ls() {
+    let config_src = r#"
+        [artifact.exec.copy]
+        command = "cp hello world"
+
+        [command.container.hello]
+        image = "alpine"
+        description = "Hello world"
+    "#;
+
+    let test_context = common::TestContext::new();
+    test_context.write_config(config_src);
+
+    let mut cmd = test_context.get_command();
+    cmd.arg("ls");
+
+    cmd.assert().success().stdout(predicate::eq(
+        "artifact.exec.copy - \ncommand.container.hello - Hello world\n",
+    ));
+}
